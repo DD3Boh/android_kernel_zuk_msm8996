16b9fdb8e2faff1e243322120a3aa8f47c5b8712
Kevin Cernekee <cernekee@gmail.com>
2014-11-06 22:44:17 -0800

    genirq: Generic chip: Change irq_reg_{readl,writel} arguments
    [ Upstream commit 332fd7c4fef5f3b166e93decb07fd69eb24f7998 ]
    
    Pass in the irq_chip_generic struct so we can use different readl/writel
    settings for each irqchip driver, when appropriate.  Compute
    (gc->reg_base + reg_offset) in the helper function because this is pretty
    much what all callers want to do anyway.
    
    Compile-tested using the following configurations:
    
        at91_dt_defconfig (CONFIG_ATMEL_AIC_IRQ=y)
        sama5_defconfig (CONFIG_ATMEL_AIC5_IRQ=y)
        sunxi_defconfig (CONFIG_ARCH_SUNXI=y)
    
    tb10x (ARC) is untested.
    
    Signed-off-by: Kevin Cernekee <cernekee@gmail.com>
    Acked-by: Thomas Gleixner <tglx@linutronix.de>
    Acked-by: Acked-by: Arnd Bergmann <arnd@arndb.de>
    Link: https://lkml.kernel.org/r/1415342669-30640-3-git-send-email-cernekee@gmail.com
    Signed-off-by: Jason Cooper <jason@lakedaemon.net>
    Signed-off-by: Sasha Levin <alexander.levin@verizon.com>
    Signed-off-by: dd3boh <dade.garberi@gmail.com>

--- include/linux/irq.h
+++ include/linux/irq.h
@@ -851,19 +851,13 @@ static inline void irq_gc_unlock(struct irq_chip_generic *gc) { }
 static inline void irq_reg_writel(struct irq_chip_generic *gc,
 				  u32 val, int reg_offset)
 {
-	if (gc->reg_writel)
-		gc->reg_writel(val, gc->reg_base + reg_offset);
-	else
-		writel(val, gc->reg_base + reg_offset);
+	writel(val, gc->reg_base + reg_offset);
 }
 
 static inline u32 irq_reg_readl(struct irq_chip_generic *gc,
 				int reg_offset)
 {
-	if (gc->reg_readl)
-		return gc->reg_readl(gc->reg_base + reg_offset);
-	else
-		return readl(gc->reg_base + reg_offset);
+	return readl(gc->reg_base + reg_offset);
 }
 
 #endif /* _LINUX_IRQ_H */
