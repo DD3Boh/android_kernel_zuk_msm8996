fa1a0b19a135820434c716124bcc8ffaf1a113b3
Steve Kondik <steve@cyngn.com>
2015-07-11 05:49:30 -0700

    video: mdss: LiveDisplay driver
     * Bring in a lightweight version of the LiveDisplay driver which
       works for this chipset.
     * This version of the driver does not include the generic
       RGB offset postprocessing, since we're going to do this using
       the official QDCM APIs.
    
    Change-Id: Ifcaaf93d56933d019d1f469fd70b8466cd4ad3e7
    Signed-off-by: dd3boh <dade.garberi@gmail.com>

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -25,6 +25,7 @@
 
 #include "mdss_dsi.h"
 #include "mdss_dba_utils.h"
+#include "mdss_livedisplay.h"
 
 #include <linux/irq.h>
 #include <linux/gpio.h>
@@ -222,7 +223,7 @@ int mdss_dsi_panel_cmd_read(struct mdss_dsi_ctrl_pdata *ctrl, char cmd0,
 	return mdss_dsi_cmdlist_put(ctrl, &cmdreq);
 }
 
-static void mdss_dsi_panel_cmds_send(struct mdss_dsi_ctrl_pdata *ctrl,
+void mdss_dsi_panel_cmds_send(struct mdss_dsi_ctrl_pdata *ctrl,
 			struct dsi_panel_cmds *pcmds, u32 flags)
 {
 	struct dcs_cmd_req cmdreq;
@@ -1069,6 +1070,11 @@ static int mdss_dsi_panel_on(struct mdss_panel_data *pdata)
 
 	if (ctrl->ds_registered)
 		mdss_dba_utils_video_on(pinfo->dba_data, pinfo);
+
+	if (pdata->event_handler)
+		pdata->event_handler(pdata, MDSS_EVENT_UPDATE_LIVEDISPLAY,
+				(void *)(unsigned long) MODE_UPDATE_ALL);
+
 end:
 	pr_debug("%s:-\n", __func__);
 	return ret;
@@ -1194,7 +1200,7 @@ static void mdss_dsi_parse_trigger(struct device_node *np, char *trigger,
 }
 
 
-static int mdss_dsi_parse_dcs_cmds(struct device_node *np,
+int mdss_dsi_parse_dcs_cmds(struct device_node *np,
 		struct dsi_panel_cmds *pcmds, char *cmd_key, char *link_key)
 {
 	const char *data;
@@ -2887,6 +2893,8 @@ static int mdss_panel_parse_dt(struct device_node *np,
 		strlcpy(ctrl_pdata->bridge_name, bridge_chip_name,
 			MSM_DBA_CHIP_NAME_MAX_LEN);
 	}
+
+	mdss_livedisplay_parse_dt(np, pinfo);
 
 	return 0;
 
