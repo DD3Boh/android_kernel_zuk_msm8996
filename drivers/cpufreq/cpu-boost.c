7eed56e985a8489df131ae2fc611afe120fa5def
Pranav Vashi <neobuddy89@gmail.com>
2016-02-16 12:24:33 +0530

    cpufreq: cpu-boost: Disable sched boost
    * Also, auto-disable sched boost if HMP is not enabled.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: dd3boh <dade.garberi@gmail.com>

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -221,10 +221,12 @@ static void do_input_boost(struct work_struct *work)
 	/* Enable scheduler boost to migrate tasks to big cluster */
 	if (sched_boost_on_input) {
 		ret = sched_set_boost(1);
-		if (ret)
+		if (ret) {
+			sched_boost_on_input = false;
 			pr_err("cpu-boost: HMP boost enable failed\n");
-		else
+		} else {
 			sched_boost_active = true;
+		}
 	}
 
 	queue_delayed_work(cpu_boost_wq, &input_boost_rem,
