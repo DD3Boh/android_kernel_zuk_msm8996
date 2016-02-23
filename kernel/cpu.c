ec47b9d0dfbfff4ea1affb474fc5250a8d6df316
Nick Desaulniers <ndesaulniers@google.com>
2016-02-23 14:12:49 -0800

    Silences WLAN, PCIe, and CPU suspend state kernel messages
    Bug: 27134656
    Change-Id: I681ec2171472514489365ca4bfc4ef16d9b344fe
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>
    Signed-off-by: dd3boh <dade.garberi@gmail.com>

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -561,6 +561,8 @@ int disable_nonboot_cpus(void)
 	 */
 	cpumask_clear(frozen_cpus);
 
+	sched_set_boost(0);
+
 	pr_debug("Disabling non-boot CPUs ...\n");
 	for_each_online_cpu(cpu) {
 		if (cpu == first_cpu)
@@ -615,13 +617,7 @@ void __ref enable_nonboot_cpus(void)
 		error = _cpu_up(cpu, 1);
 		trace_suspend_resume(TPS("CPU_ON"), cpu, false);
 		if (!error) {
-			pr_info("CPU%d is up\n", cpu);
-			cpu_device = get_cpu_device(cpu);
-			if (!cpu_device)
-				pr_err("%s: failed to get cpu%d device\n",
-				       __func__, cpu);
-			else
-				kobject_uevent(&cpu_device->kobj, KOBJ_ONLINE);
+			pr_debug("CPU%d is up\n", cpu);
 			continue;
 		}
 		pr_warn("Error taking CPU%d up: %d\n", cpu, error);
