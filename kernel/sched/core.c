45bb170a3c214a165d55614f20bb7527fee3104d
Peter Zijlstra <peterz@infradead.org>
2016-10-03 16:20:59 +0200

    UPSTREAM: sched/core: Add missing update_rq_clock() in post_init_entity_util_avg()
    Address this rq-clock update bug:
    
      WARNING: CPU: 0 PID: 0 at ../kernel/sched/sched.h:797 post_init_entity_util_avg()
      rq->clock_update_flags < RQCF_ACT_SKIP
    
      Call Trace:
        __warn()
        post_init_entity_util_avg()
        wake_up_new_task()
        _do_fork()
        kernel_thread()
        rest_init()
        start_kernel()
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Mike Galbraith <efault@gmx.de>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: linux-kernel@vger.kernel.org
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    (cherry picked from commit 4126bad6717336abe5d666440ae15555563ca53f)
    Change-Id: Ibe9a73386896377f96483d195e433259218755a5
    Signed-off-by: Brendan Jackman <brendan.jackman@arm.com>
    Signed-off-by: Chris Redpath <chris.redpath@arm.com>
    Signed-off-by: Davide Garberi <dade.garberi@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -2703,6 +2703,7 @@ void wake_up_new_task(struct task_struct *p)
 	__set_task_cpu(p, select_task_rq(p, task_cpu(p), SD_BALANCE_FORK, 0, 1));
 #endif
 	rq = __task_rq_lock(p);
+	update_rq_clock(rq);
 	mark_task_starting(p);
 	walt_mark_task_starting(p);
 	update_rq_clock(rq);
