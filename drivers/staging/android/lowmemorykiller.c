c47c80dec2213260a4dbc2c83a56af8a564d65bd
myfluxi <linflux@arcor.de>
2016-12-07 19:34:31 +0100

    lowmemorykiller: Account for unevictable pages in vmpressure notifier
    Change-Id: Ic37926b0902bd4b850b070c8fceea8f328cd7b0d
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -154,6 +154,7 @@ static int lmk_vmpressure_notifier(struct notifier_block *nb,
 	if (pressure >= 95) {
 		other_file = global_page_state(NR_FILE_PAGES) + zcache_pages() -
 			global_page_state(NR_SHMEM) -
+			global_page_state(NR_UNEVICTABLE) -
 			total_swapcache_pages();
 		other_free = global_page_state(NR_FREE_PAGES);
 
@@ -167,6 +168,7 @@ static int lmk_vmpressure_notifier(struct notifier_block *nb,
 
 		other_file = global_page_state(NR_FILE_PAGES) + zcache_pages() -
 			global_page_state(NR_SHMEM) -
+			global_page_state(NR_UNEVICTABLE) -
 			total_swapcache_pages();
 
 		other_free = global_page_state(NR_FREE_PAGES);
