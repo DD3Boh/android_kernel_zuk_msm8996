bf742c8545077e97c452429f2da6dbbb81175fca
Ondrej Hlavac <hlavaco@gmail.com>
2017-03-11 22:46:31 +0100

    power: qpnp-smcharger: fix hvdcp voters
    as they were random, introduced by https://github.com/ontherunvaro/android_kernel_zuk_msm8996/commit/add80bdd655d494c9c2ec758a2971a9ab227c58b

--- drivers/power/qpnp-smbcharger.c
+++ drivers/power/qpnp-smbcharger.c
@@ -4589,7 +4589,7 @@ static int smbchg_external_otg_regulator_disable(struct regulator_dev *rdev)
 	 * input.
 	 */
 #ifdef SUPPORT_ONLY_5V_CHARGER
-	rc = vote(chip->hvdcp_enable_votable, HVDCP_PMIC_VOTER, true, 0);
+	rc = vote(chip->hvdcp_enable_votable, HVDCP_OTG_VOTER, true, 0);
 	if (rc < 0) {
 		dev_err(chip->dev, "Couldn't disable HVDCP rc=%d\n", rc);
 		return rc;
@@ -4599,7 +4599,7 @@ static int smbchg_external_otg_regulator_disable(struct regulator_dev *rdev)
 				chip->usb_chgpth_base + USBIN_CHGR_CFG,
 				0xFF, USBIN_ADAPTER_5V);
 #else
-	rc = vote(chip->hvdcp_enable_votable, HVDCP_PULSING_VOTER, false, 1);
+	rc = vote(chip->hvdcp_enable_votable, HVDCP_OTG_VOTER, false, 1);
 	if (rc < 0) {
 		dev_err(chip->dev, "Couldn't enable HVDCP rc=%d\n", rc);
 		return rc;
@@ -6045,7 +6045,7 @@ static int smbchg_unprepare_for_pulsing(struct smbchg_chip *chip)
 #ifdef SUPPORT_ONLY_5V_CHARGER
 	/* disable HVDCP */
 	pr_smb(PR_MISC, "Disable HVDCP\n");
-	rc = vote(chip->hvdcp_enable_votable, HVDCP_PMIC_VOTER, true, 0);
+	rc = vote(chip->hvdcp_enable_votable, HVDCP_PULSING_VOTER, true, 0);
 	if (rc < 0) {
 		pr_err("Couldn't disable HVDCP rc=%d\n", rc);
 		return rc;
