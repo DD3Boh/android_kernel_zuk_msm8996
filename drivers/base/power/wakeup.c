e90abd86da0242c60e69cdf5fe64e95690c46270
dd3boh <dade.garberi@gmail.com>
2017-05-05 13:50:43 +0200

    Revert "wakeup: add toggles for wlan wakelocks. They are all enabled by default, it's up to the user and I provide no support if Wi-Fi stops working normally without these locks enabled. This is for advanced users"
    This reverts commit 964643044f0d1943f4dc8f910c6e835bd5edd2c3.

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -16,14 +16,6 @@
 #include <linux/debugfs.h>
 #include <linux/types.h>
 #include <trace/events/power.h>
-#include <linux/moduleparam.h>
-
-static bool enable_wlan_rx_wake_ws = true;
-module_param(enable_wlan_rx_wake_ws, bool, 0644);
-static bool enable_wlan_ctrl_wake_ws = true;
-module_param(enable_wlan_ctrl_wake_ws, bool, 0644);
-static bool enable_wlan_wake_ws = true;
-module_param(enable_wlan_wake_ws, bool, 0644);
 
 #include "power.h"
 
@@ -434,15 +426,6 @@ static void wakeup_source_activate(struct wakeup_source *ws)
 	 * out of PM_SUSPEND_FREEZE state
 	 */
 	freeze_wake();
-
-	if (!enable_wlan_rx_wake_ws && !strcmp(ws->name, "wlan_rx_wake"))
-                return;
-
-	if (!enable_wlan_ctrl_wake_ws && !strcmp(ws->name, "wlan_ctrl_wake"))
-                return;
-
-	if (!enable_wlan_wake_ws && !strcmp(ws->name, "wlan_wake"))
-                return;
 
 	ws->active = true;
 	ws->active_count++;
