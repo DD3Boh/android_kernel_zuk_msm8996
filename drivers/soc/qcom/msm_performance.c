3f7c7fb66577cb20eccd1318bd9ebb9338da7859
dd3boh <dade.garberi@gmail.com>
2017-05-16 23:59:26 +0200

    Disable touch boost as default
    The touch boosts are still activable but I prefer to let them disabled as default

--- drivers/soc/qcom/msm_performance.c
+++ drivers/soc/qcom/msm_performance.c
@@ -28,7 +28,7 @@
 
 static unsigned int use_input_evts_with_hi_slvt_detect;
 static struct mutex managed_cpus_lock;
-static int touchboost = 1;
+static int touchboost = 0;
 
 /* Maximum number to clusters that this module will manage*/
 static unsigned int num_clusters;
@@ -432,7 +432,7 @@ static int set_cpu_min_freq(const char *buf, const struct kernel_param *kp)
 	const char *reset = "0:0 2:0";
 
 	if (touchboost == 0)
-		cp = reset;
+		return 0;
 
 	while ((cp = strpbrk(cp + 1, " :")))
 		ntokens++;
