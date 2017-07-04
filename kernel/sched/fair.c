1c8b235e519cd7b1a308f12d5767a332d6f15042
Chris Redpath <chris.redpath@arm.com>
2017-07-04 10:19:58 +0100

    sched/fair: Add a backup_cpu to find_best_target
    Sometimes we find a target cpu but then we do not use it as
    the energy_diff indicates that we would increase energy usage
    or not save anything. To offer an additional option for those
    cases, we return a second option which is what we would have
    selected if the target CPU had not been found. This gives us
    another chance to try to save some energy.
    
    Change-Id: I42c4f20aba10e4cf65b51ac4153e2e00e534c8c7
    Signed-off-by: Chris Redpath <chris.redpath@arm.com>
    Signed-off-by: Patrick Bellasi <patrick.bellasi@arm.com>
    Signed-off-by: Davide Garberi <dade.garberi@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7486,7 +7486,8 @@ static int start_cpu(bool boosted)
 	return boosted ? rd->max_cap_orig_cpu : rd->min_cap_orig_cpu;
 }
 
-static inline int find_best_target(struct task_struct *p, bool boosted, bool prefer_idle)
+static inline int find_best_target(struct task_struct *p, int *backup_cpu,
+				   bool boosted, bool prefer_idle)
 {
 	unsigned long best_idle_min_cap_orig = ULONG_MAX;
 	unsigned long min_util = boosted_task_util(p);
@@ -7501,6 +7502,8 @@ static inline int find_best_target(struct task_struct *p, bool boosted, bool pre
 	int best_idle_cpu = -1;
 	int target_cpu = -1;
 	int cpu, i;
+
+	*backup_cpu = -1;
 
 	schedstat_inc(p, se.statistics.nr_wakeups_fbt_attempts);
 	schedstat_inc(this_rq(), eas_stats.fbt_attempts);
@@ -7747,6 +7750,10 @@ static inline int find_best_target(struct task_struct *p, bool boosted, bool pre
 		target_cpu = prefer_idle
 			? best_active_cpu
 			: best_idle_cpu;
+	else
+		*backup_cpu = prefer_idle
+		? best_active_cpu
+		: best_idle_cpu;
 
 	schedstat_inc(p, se.statistics.nr_wakeups_fbt_count);
 	schedstat_inc(this_rq(), eas_stats.fbt_count);
@@ -7782,7 +7789,7 @@ static int
 select_energy_cpu_brute(struct task_struct *p, int prev_cpu, int sync)
 {
 	struct sched_domain *sd;
-	int target_cpu = prev_cpu, tmp_target;
+	int target_cpu = prev_cpu, tmp_target, tmp_backup;
 	bool boosted, prefer_idle;
 
 	schedstat_inc(p, se.statistics.nr_wakeups_secb_attempts);
@@ -7809,7 +7816,7 @@ select_energy_cpu_brute(struct task_struct *p, int prev_cpu, int sync)
 
 	sd = rcu_dereference(per_cpu(sd_ea, prev_cpu));
 	/* Find a cpu with sufficient capacity */
-	tmp_target = find_best_target(p, boosted, prefer_idle);
+	tmp_target = find_best_target(p, &tmp_backup, boosted, prefer_idle);
 
 	if (!sd)
 		goto unlock;
@@ -7846,10 +7853,15 @@ select_energy_cpu_brute(struct task_struct *p, int prev_cpu, int sync)
 		}
 
 		if (energy_diff(&eenv) >= 0) {
-			schedstat_inc(p, se.statistics.nr_wakeups_secb_no_nrg_sav);
-			schedstat_inc(this_rq(), eas_stats.secb_no_nrg_sav);
-			target_cpu = prev_cpu;
-			goto unlock;
+			/* No energy saving for target_cpu, try backup */
+			target_cpu = tmp_backup;
+			eenv.dst_cpu = target_cpu;
+			if (tmp_backup < 0 || energy_diff(&eenv) >= 0) {
+				schedstat_inc(p, se.statistics.nr_wakeups_secb_no_nrg_sav);
+				schedstat_inc(this_rq(), eas_stats.secb_no_nrg_sav);
+				target_cpu = prev_cpu;
+				goto unlock;
+			}
 		}
 
 		schedstat_inc(p, se.statistics.nr_wakeups_secb_nrg_sav);
