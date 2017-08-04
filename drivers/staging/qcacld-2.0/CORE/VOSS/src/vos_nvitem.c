ea3234dae20ea8dc71962ce237bea95a134a5eda
Srinivas Girigowda <sgirigow@codeaurora.org>
2017-08-04 11:22:54 -0700

    qcacld-2.0:  Add support for CFG80211_REMOVE_IEEE80211_BACKPORT
    CFG80211_REMOVE_IEEE80211_BACKPORT is added to indicate
    backport of Change-Id: 57fbcce37be7c1d2622b56587c10ade00e96afa3
    from upstream 4.7 kernel to 4.4.
    
    Hence add this flag CFG80211_REMOVE_IEEE80211_BACKPORT to
    support this compilation. Also introduce HDD_NUM_NL80211_BANDS.
    
    Signed-off-by: Subhajeet Muhuri <kenny3fcb@gmail.com>

--- drivers/staging/qcacld-2.0/CORE/VOSS/src/vos_nvitem.c
+++ drivers/staging/qcacld-2.0/CORE/VOSS/src/vos_nvitem.c
@@ -1876,7 +1876,7 @@ int vos_update_band(v_U8_t  band_capability)
 		return VOS_STATUS_E_FAULT;
 	}
 
-	for (i = 0; i < NUM_NL80211_BANDS; i++) {
+	for (i = 0; i < HDD_NUM_NL80211_BANDS; i++) {
 		if (NULL == wiphy->bands[i])
 			continue;
 
@@ -1960,7 +1960,7 @@ static int create_linux_regulatory_entry(struct wiphy *wiphy,
         VOS_TRACE(VOS_MODULE_ID_VOSS, VOS_TRACE_LEVEL_INFO,
                   "BandCapability is set to 2G only");
 
-    for (i = 0, m = 0; i<NUM_NL80211_BANDS; i++)
+    for (i = 0, m = 0; i<HDD_NUM_NL80211_BANDS; i++)
     {
         if (wiphy->bands[i] == NULL)
             continue;
@@ -2271,7 +2271,7 @@ static void restore_custom_reg_settings(struct wiphy *wiphy)
 	struct ieee80211_channel *chan;
 	int i;
 
-	for (band = 0; band < NUM_NL80211_BANDS; band++) {
+	for (band = 0; band < HDD_NUM_NL80211_BANDS; band++) {
 		sband = wiphy->bands[band];
 		if (!sband)
 			continue;
@@ -2693,7 +2693,7 @@ VOS_STATUS vos_init_wiphy_from_nv_bin(void)
 #endif
     }
     m = 0;
-    for (i = 0; i < NUM_NL80211_BANDS; i++)
+    for (i = 0; i < HDD_NUM_NL80211_BANDS; i++)
     {
 
         if (wiphy->bands[i] == NULL)
