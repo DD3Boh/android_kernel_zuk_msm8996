a8f5e7e4f6f9c15953fea294caf6e415ce88db5e
Davide Garberi <dade.garberi@gmail.com>
2017-11-26 17:19:17 +0100

    include: Wrap ZUK's headers camera edits under ifdefs
    Signed-off-by: Davide Garberi <dade.garberi@gmail.com>

--- include/uapi/media/msmb_isp.h
+++ include/uapi/media/msmb_isp.h
@@ -367,11 +367,22 @@ enum msm_vfe_axi_stream_cmd {
 	STOP_IMMEDIATELY,
 };
 
+#ifndef CONFIG_ZUK_CAMERA
+enum msm_vfe_hw_state {
+	HW_STATE_NONE,
+	HW_STATE_SLEEP,
+	HW_STATE_AWAKE,
+};
+#endif
+
 struct msm_vfe_axi_stream_cfg_cmd {
 	uint8_t num_streams;
 	uint32_t stream_handle[VFE_AXI_SRC_MAX];
 	enum msm_vfe_axi_stream_cmd cmd;
 	uint8_t sync_frame_id_src;
+#ifndef CONFIG_ZUK_CAMERA
+	enum msm_vfe_hw_state hw_state;
+#endif
 };
 
 enum msm_vfe_axi_stream_update_type {
@@ -495,6 +506,9 @@ enum msm_vfe_reg_cfg_type {
 	VFE_HW_UPDATE_UNLOCK,
 	SET_WM_UB_SIZE,
 	SET_UB_POLICY,
+#ifndef CONFIG_ZUK_CAMERA
+	GET_VFE_HW_LIMIT,
+#endif
 };
 
 struct msm_vfe_cfg_cmd2 {
@@ -946,6 +960,9 @@ enum msm_isp_ioctl_cmd_code {
 	MSM_ISP_UNMAP_BUF,
 	MSM_ISP_FETCH_ENG_MULTI_PASS_START,
 	MSM_ISP_MAP_BUF_START_MULTI_PASS_FE,
+#ifndef CONFIG_ZUK_CAMERA
+	MSM_ISP_CFG_HW_STATE,
+#endif
 	MSM_ISP_AHB_CLK_CFG,
 	MSM_ISP_UPDATE_FE_FRAME_ID,
 	MSM_ISP_RESTART_FE,
@@ -1071,6 +1088,12 @@ enum msm_isp_ioctl_cmd_code {
 #define VIDIOC_MSM_ISP_MAP_BUF_START_MULTI_PASS_FE \
 	_IOWR('V', MSM_ISP_MAP_BUF_START_MULTI_PASS_FE, \
 		struct msm_vfe_fetch_eng_multi_pass_start)
+
+#ifndef CONFIG_ZUK_CAMERA
+#define VIDIOC_MSM_ISP_CFG_HW_STATE \
+	_IOWR('V', MSM_ISP_CFG_HW_STATE, \
+		struct msm_vfe_axi_stream_cfg_cmd)
+#endif
 
 #define VIDIOC_MSM_ISP_AHB_CLK_CFG \
 	_IOWR('V', MSM_ISP_AHB_CLK_CFG, struct msm_isp_ahb_clk_cfg)
