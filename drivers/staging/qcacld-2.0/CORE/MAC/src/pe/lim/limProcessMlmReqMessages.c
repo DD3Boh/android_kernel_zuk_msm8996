5b03d2946676480b71badfa470cdce10fc56ebeb
Nathan Chancellor <natechancellor@gmail.com>
2018-05-09 20:29:59 -0700

    qcacld-2.0: Remove unnecessary parentheses
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- drivers/staging/qcacld-2.0/CORE/MAC/src/pe/lim/limProcessMlmReqMessages.c
+++ drivers/staging/qcacld-2.0/CORE/MAC/src/pe/lim/limProcessMlmReqMessages.c
@@ -4523,9 +4523,8 @@ ePhyChanBondState limGet11ACPhyCBState(tpAniSirGlobal pMac, tANI_U8 channel, tAN
         return htSecondaryChannelOffset;
     }
 
-    if ( (htSecondaryChannelOffset
+    if (htSecondaryChannelOffset
                  == PHY_DOUBLE_CHANNEL_LOW_PRIMARY)
-       )
     {
         if ((channel + 2 ) == peerCenterChan )
             cbState =  PHY_QUADRUPLE_CHANNEL_20MHZ_LOW_40MHZ_CENTERED;
@@ -4538,9 +4537,8 @@ ePhyChanBondState limGet11ACPhyCBState(tpAniSirGlobal pMac, tANI_U8 channel, tAN
                        FL("Invalid Channel Number = %d Center Chan = %d "),
                                  channel, peerCenterChan);
     }
-    if ( (htSecondaryChannelOffset
+    if (htSecondaryChannelOffset
                  == PHY_DOUBLE_CHANNEL_HIGH_PRIMARY)
-       )
     {
         if ((channel - 2 ) == peerCenterChan )
             cbState =  PHY_QUADRUPLE_CHANNEL_20MHZ_HIGH_40MHZ_CENTERED;
