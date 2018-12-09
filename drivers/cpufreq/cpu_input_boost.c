31e1e430b3c914bb9a606ffe21823a79cfb37f5c
kdrag0n <dragon@khronodragon.com>
2018-12-09 11:35:50 -0800

    msm: mdss: Boost only if last interaction was within 1s
    This should prevent AOD notification boosting and excessive boosting
    when watching videos.
    
    Signed-off-by: kdrag0n <dragon@khronodragon.com>
    Signed-off-by: Subhajeet Muhuri <kenny3fcb@gmail.com>

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -20,6 +20,8 @@
 #include <linux/moduleparam.h>
 #include <linux/slab.h>
 
+unsigned long last_input_time;
+
 static unsigned int input_boost_freq_lp = CONFIG_INPUT_BOOST_FREQ_LP;
 static unsigned int input_boost_freq_hp = CONFIG_INPUT_BOOST_FREQ_PERF;
 static unsigned short input_boost_duration = CONFIG_INPUT_BOOST_DURATION_MS;
@@ -260,6 +262,8 @@ static void cpu_input_boost_input_event(struct input_handle *handle,
 		return;
 
 	queue_work(b->wq, &b->input_boost);
+
+	last_input_time = jiffies;
 }
 
 static int cpu_input_boost_input_connect(struct input_handler *handler,
--- drivers/video/fbdev/msm/mdss_fb.c
+++ drivers/video/fbdev/msm/mdss_fb.c
@@ -5060,8 +5060,10 @@ int mdss_fb_do_ioctl(struct fb_info *info, unsigned int cmd,
 		ret = mdss_fb_mode_switch(mfd, dsi_mode);
 		break;
 	case MSMFB_ATOMIC_COMMIT:
-		cpu_input_boost_kick();
-		devfreq_boost_kick(DEVFREQ_MSM_CPUBW);
+ 		if (time_before(jiffies, last_input_time + msecs_to_jiffies(1000))) {
+			cpu_input_boost_kick();
+			devfreq_boost_kick(DEVFREQ_MSM_CPUBW);
+		}
 		ret = mdss_fb_atomic_commit_ioctl(info, argp, file);
 		break;
 
